bf6d45fc349891d1f6b9ef06a5e2225d72d83ab2
helenajullia <helenajulliapastore@yahoo.com>
2024-10-26 10:24:39 +0300

    Initial commit: Setup Spring Boot backend with PostgreSQL configuration

---
+++ PartyHubBackend/src/main/java/com/example/PartyHubBackend/PartyHubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.PartyHubBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartyHubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartyHubBackendApplication.class, args);
+	}
+
+}
---
+++ PartyHubBackend/src/test/java/com/example/PartyHubBackend/PartyHubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PartyHubBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartyHubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
